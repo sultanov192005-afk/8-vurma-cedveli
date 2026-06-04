e8d96d5b715cdb5c1e5a6aecdc7e84584da424f3
sultanov192005-afk <sultanov192005@gmail.com>
2026-06-04 21:01:12 +0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     for (let x = 0; x <= 9; x++) {
         for (let y = 0; y <= 9; y++) {
-            basic.showString("" + "")
+            basic.showString("" + (x + 1) + "x" + (x + 1) + "=" + (x + 1 * (y + 1)))
         }
     }
 })
